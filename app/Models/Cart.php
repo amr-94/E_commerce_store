0701a4ff268e14640345a2386a4612430b0e6deb
amr hamdy <amrhamdy15@yahoo.com>
2024-05-15 01:04:20 +0300

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -6,7 +6,6 @@ use App\Observers\CartObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Str;
 
 class Cart extends Model
 {
